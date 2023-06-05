363d90af484f79ac5f5c98365d875923584b8877
Shahriyar Imtiyaz Shehab <42363048+shahriyarshehab@users.noreply.github.com>
2023-06-05 13:08:48 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+function showClock(){let h=document.getElementsByClassName('hr')[0];let m=document.getElementsByClassName('mn')[0];let s=document.getElementsByClassName('ss')[0];let date=new Date();let hours=date.getHours();let minutes=date.getMinutes();let second=date.getSeconds();h.style.transform=`rotate(${30*hours + minutes/2}deg)`;m.style.transform=`rotate(${6*minutes}deg)`;s.style.transform=`rotate(${6*second}deg)`}
+setInterval(showClock,1000)
